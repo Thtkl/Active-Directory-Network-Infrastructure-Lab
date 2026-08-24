5d53189f55d78dfb390042193e8e9aa5601ed2ee
Thtkl <anildota3@gmail.com>
2026-08-24 05:56:32 +0200

    automated user adding script for powershell

---
+++ user_adding.ps1
@@ -0,0 +1,23 @@
+﻿$userPass = "123Password!"
+$names = Get-Content "C:\Users\aniltkl\Desktop\usernames.txt"
+
+$password = ConvertTo-SecureString $userPass -AsPlainText -Force
+$domain   = (Get-ADDomain).DistinguishedName
+
+foreach ($line in $names) {
+    $first = $line.Trim().Split(" ")[0].ToLower()
+    $last  = $line.Trim().Split(" ")[1].ToLower()
+    $user  = "$($first[0])$last"
+    
+    Write-Host "Creating user: $user"
+    
+    New-ADUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $user `
+               -Name $user `
+               -EmployeeID $user `
+               -PasswordNeverExpires $true `
+               -Path "OU=1USERS,$domain" `
+               -Enabled $true
+}
